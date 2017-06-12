9375c9769412b9a826f79429c669a15cf3587ccd
Galina Atamankina <g.atamankina@win.tu-berlin.de>
2017-06-12 13:24:19 +0200

    Project configs and Simple App

---
+++ src/com/example/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.example;
+
+/**
+ * @author Galina Atamankina
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/example/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Galina Atamankina
+ *
+ */
+package com.example;

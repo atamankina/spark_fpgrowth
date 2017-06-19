069fd35a50c2d8be9521491c2ff7922d069ed5b6
Galina Atamankina <g.atamankina@win.tu-berlin.de>
2017-06-19 23:45:42 +0200

    Cut off header of CSV

--- src/com/example/FPGrowthPreprocess.java
+++ src/com/example/FPGrowthPreprocess.java
@@ -9,9 +9,12 @@ import java.util.List;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.DataFrameReader;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
+
+import scala.Tuple2;
 
 import org.apache.spark.mllib.fpm.AssociationRules;
 import org.apache.spark.mllib.fpm.FPGrowth;
@@ -25,13 +28,24 @@ public class FPGrowthPreprocess {
 
 	public static void main(String[] args) {
 		
+		String inputpath = "./data/OnlineRetailShort.csv";
+		String temppath = "./data/OnlineRetailTemp";
+		String transactionspath = "./data/OnlineRetailTrans";
+		
 		SparkConf conf = new SparkConf()
 				.setAppName("FPGrowth with Preprocessing")
+		//this config is only good for quick local testing, otherwise passed in command line
 				.setMaster("local[*]");
-		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaSparkContext jsc = new JavaSparkContext(conf);
 		
-		JavaRDD<String> data = sc.textFile("./data/OnlineRetailShort.csv");
+		//take necessary CSV as input
+		JavaRDD<String> input = jsc.textFile(inputpath);
 		
+		//ignore first line in CSV
+		String header = input.first();
+		JavaRDD<String> data = input.filter((String row) -> !row.equals(header));
+	
+	 //   JavaRDD<Tuple2<String, String>> tuples = jsc.textFile(temppath).map();
 		
 		
 

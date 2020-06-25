78963942e47ebe4e8a2c582369ff7a9720f4eb61
Galina Atamankina <g.atamankina@gmail.com>
2020-06-25 21:34:51 +0200

    Remove apriori and eclat.

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.example;
 
@@ -19,7 +19,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		String inputPath = "./data/OnlineRetailShort.csv";
 		String pairsPath = "./data/OnlineRetailPairs";
 		String pairsPathMapped = "./data/OnlineRetailPairsMapped";
@@ -27,44 +27,41 @@ public class Main {
 		String transactionsMappedPath = "./data/OnlineRetailTransMapped";
 		String frequentItemsPath = "./data/OnlineRetailFrequentItems";
 		String synthetic = "./data/SyntheticData.txt";
-		
+
 		/** set Spark context */
 		SparkConf conf = new SparkConf()
 				.setAppName("Mining Frequent Items with Preprocessing")
 				.setMaster("local[*]");
 		JavaSparkContext jsc = new JavaSparkContext(conf);
-		
+
 		/** initialize transformer class */
 		MarketBasketTransformer mbt = new MarketBasketTransformer();
 		mbt.setJsc(jsc);
-		
+
 		/** transform input */
 		JavaRDD<List<String>> transactions = mbt.transformToMarketBasketLists(inputPath, 1, 0, 1);
 		transactions.saveAsTextFile(transactionsPath);
-		
-		/** run algorithm on the transformed input */		
+
+		/** run algorithm on the transformed input */
 		FPGrowthRunner.runFPGrowth(transactions, 0.2, 1, frequentItemsPath);
-		
-		
-		
-		
+
+
+
+
 		//JavaRDD<String> synt = jsc.textFile(synthetic, 1);
 		
-		//Eclat.runEclat(jsc, transmapped, 1);
-		//Apriori.runApriori(jsc, transmapped, 3);
-		
-		
+
 		//JavaRDD<String> transactions = mbt.transformToMarketBasketLines(inputPath, 1, 0, 1);
-		
+
 		//JavaRDD<String> transmapped = mbt.transformToMarketBasketLinesMapped(inputPath, 1, 0, 1);
 		//mbt.transformToTwoTuples(inputPath, 1, 0, 1).saveAsTextFile(pairsPath);
 		//mbt.transformToTwoTuplesMapped(inputPath, 1, 0, 1).saveAsTextFile(pairsPathMapped);
-		
+
 		//mbt.countUniquValues(inputPath, 1, 1).saveAsTextFile(transactionsPath);
-		
+
 		//transmapped.saveAsTextFile(transactionsMappedPath);
-		
-		
+
+
 		jsc.stop();
 
 	}
